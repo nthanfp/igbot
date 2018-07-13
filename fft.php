a5cdc34c6cbed1e0dea5f8976fdd99623a564f57
Nathanael Ferry P <ntnferry1505@gmail.com>
2018-07-13 20:35:21 +0700

    Update fft.php

--- fft.php
+++ fft.php
@@ -51,96 +51,99 @@ if($ext->status <> 'ok') {
     $jumlah = 10;
     echo ">> Input Delay (in seconds) : ";
     $delay = trim(fgets(STDIN, 1024));
-    echo ">> Please Wait....\n";
-    // end
-    // start
-    $idtarget = getuid($target);
-    $getinfo  = proccess(1, $useragent, 'users/' . $idtarget . '/info/', $cookie);
-    $getinfo  = json_decode($getinfo[1]);
-    if($tipex):
-        $tipenya = 'followers';
-    else:
-        $tipenya = 'following';
-    endif;
-    if($tipenya == 'followers'):
-        if(!is_numeric($jumlah))
-            $limit = 1;
-        elseif($jumlah > ($getinfo->user->follower_count - 1))
-            $limit = $getinfo->user->follower_count - 1;
-        else
-            $limit = $jumlah - 1;
-        $tipe = 'followers';
-    else:
-        if(!is_numeric($jumlah))
-            $limit = 1;
-        elseif($jumlah > ($getinfo->user->following_count - 1))
-            $limit = $getinfo->user->following_count - 1;
-        else
-            $limit = $jumlah - 1;
-        $tipe = 'following';
-    endif;
-    $c       = 0;
-    $listids = array();
+    $iyh   = true;
     do {
-        $parameters = ($c > 0) ? '?max_id=' . $c : '';
-        $req        = proccess(1, $useragent, 'friendships/' . $idtarget . '/' . $tipe . '/' . $parameters, $cookie);
-        $req        = json_decode($req[1]);
-        for($i = 0; $i < count($req->users); $i++):
-            if(count($listids) <= $limit)
-                $listids[count($listids)] = $req->users[$i]->pk;
-        endfor;
-        $c = (isset($req->next_max_id)) ? $req->next_max_id : 0;
-    } while(count($listids) <= $limit);
-    for($i = 0; $i < count($listids); $i++):
-    //user details
-        $getx = proccess(1, $useragent, 'users/' . $listids[$i] . '/info/', $cookie);
-        $getx = json_decode($getx[1]);
-        $priv = $getx->user->is_private;
-        if($priv == 1):
-            echo "@" . $req->users[$i]->username . "User Private, Skipp\n";
+        echo ">> Please Wait....\n";
+        // end
+        // start
+        $idtarget = getuid($target);
+        $getinfo  = proccess(1, $useragent, 'users/' . $idtarget . '/info/', $cookie);
+        $getinfo  = json_decode($getinfo[1]);
+        if($tipex):
+            $tipenya = 'followers';
         else:
-            //follow user
-            $follow = proccess(1, $useragent, 'friendships/create/' . $listids[$i] . '/', $cookie, hook('{"user_id":"' . $listids[$i] . '"}'));
-            $follow = json_decode($follow[1]);
-            if($follow->status == 'ok'):
-                $follow_status = 'Success follow';
-            else:
-                $follow_status = 'Failed follow';
-            endif;
-            //get new media user
-            $getmedia = proccess(1, $useragent, 'feed/user/' . $listids[$i] . '/', $cookie);
-            $getmedia = json_decode($getmedia[1], true);
-            $mediaId  = $getmedia['items'][0]['id'];
-            //like media
-            $like     = proccess(1, $useragent, 'media/' . $mediaId . '/like/', $cookie, hook('{"media_id":"' . $mediaId . '"}'), array(
-                'Accept-Language: id-ID, en-US',
-                'X-IG-Connection-Type: WIFI'
-            ));
-            $like     = json_decode($like[1]);
-            if($like->status == 'ok'):
-                $like_status = 'Liked';
-            else:
-                $like_status = 'Failed Like';
-            endif;
-            //comment media
-            $listKomentar = array(
-                "Follow Back ya kak 😘",
-                "Follow aku ya kak 😁",
-                "Bagus banget kak fotonya 😍,Follow back yaaa",
-                "Caption on point banget!!, Follow back ya 😊",
-                "Follback dong kak😄"
-            );
-            $commentAcak  = $listKomentar[rand(0, count($listKomentar) - 1)];
-            $comment      = proccess(1, $useragent, 'media/' . $mediaId . '/comment/', $cookie, hook('{"comment_text":"' . $commentAcak . '"}'));
-            $comment      = json_decode($comment[1]);
-            if($comment->status == 'ok'):
-                $comment_status = 'Comment';
-            else:
-                $comment_status = 'Failed Comment';
-            endif;
-            echo $follow_status . " " . $req->users[$i]->username . " | ". $like_status ." ".$mediaId." | ".$commentAcak."\n";
-            sleep($delay);
+            $tipenya = 'following';
         endif;
-    endfor;
+        if($tipenya == 'followers'):
+            if(!is_numeric($jumlah))
+                $limit = 1;
+            elseif($jumlah > ($getinfo->user->follower_count - 1))
+                $limit = $getinfo->user->follower_count - 1;
+            else
+                $limit = $jumlah - 1;
+            $tipe = 'followers';
+        else:
+            if(!is_numeric($jumlah))
+                $limit = 1;
+            elseif($jumlah > ($getinfo->user->following_count - 1))
+                $limit = $getinfo->user->following_count - 1;
+            else
+                $limit = $jumlah - 1;
+            $tipe = 'following';
+        endif;
+        $c       = 0;
+        $listids = array();
+        do {
+            $parameters = ($c > 0) ? '?max_id=' . $c : '';
+            $req        = proccess(1, $useragent, 'friendships/' . $idtarget . '/' . $tipe . '/' . $parameters, $cookie);
+            $req        = json_decode($req[1]);
+            for($i = 0; $i < count($req->users); $i++):
+                if(count($listids) <= $limit)
+                    $listids[count($listids)] = $req->users[$i]->pk;
+            endfor;
+            $c = (isset($req->next_max_id)) ? $req->next_max_id : 0;
+        } while(count($listids) <= $limit);
+        for($i = 0; $i < count($listids); $i++):
+        //user details
+            $getx = proccess(1, $useragent, 'users/' . $listids[$i] . '/info/', $cookie);
+            $getx = json_decode($getx[1]);
+            $priv = $getx->user->is_private;
+            if($priv == 1):
+                echo "@" . $req->users[$i]->username . "User Private, Skipp\n";
+            else:
+                //follow user
+                $follow = proccess(1, $useragent, 'friendships/create/' . $listids[$i] . '/', $cookie, hook('{"user_id":"' . $listids[$i] . '"}'));
+                $follow = json_decode($follow[1]);
+                if($follow->status == 'ok'):
+                    $follow_status = 'Success follow';
+                else:
+                    $follow_status = 'Failed follow';
+                endif;
+                //get new media user
+                $getmedia = proccess(1, $useragent, 'feed/user/' . $listids[$i] . '/', $cookie);
+                $getmedia = json_decode($getmedia[1], true);
+                $mediaId  = $getmedia['items'][0]['id'];
+                //like media
+                $like     = proccess(1, $useragent, 'media/' . $mediaId . '/like/', $cookie, hook('{"media_id":"' . $mediaId . '"}'), array(
+                    'Accept-Language: id-ID, en-US',
+                    'X-IG-Connection-Type: WIFI'
+                ));
+                $like     = json_decode($like[1]);
+                if($like->status == 'ok'):
+                    $like_status = 'Liked';
+                else:
+                    $like_status = 'Failed Like';
+                endif;
+                //comment media
+                $listKomentar = array(
+                    "Follow Back ya kak 😘",
+                    "Follow aku ya kak 😁",
+                    "Bagus banget kak fotonya 😍,Follow back yaaa",
+                    "Caption on point banget!!, Follow back ya 😊",
+                    "Follback dong kak😄"
+                );
+                $commentAcak  = $listKomentar[rand(0, count($listKomentar) - 1)];
+                $comment      = proccess(1, $useragent, 'media/' . $mediaId . '/comment/', $cookie, hook('{"comment_text":"' . $commentAcak . '"}'));
+                $comment      = json_decode($comment[1]);
+                if($comment->status == 'ok'):
+                    $comment_status = 'Comment';
+                else:
+                    $comment_status = 'Failed Comment';
+                endif;
+                echo $follow_status . " " . $req->users[$i]->username . " | " . $like_status . " " . $mediaId . " | " . $commentAcak . "\n";
+                sleep($delay);
+            endif;
+        endfor;
+    } while($iyh == 'false');
 }
 ?>
