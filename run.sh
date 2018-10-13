7d615426b80bcd36bcd71617e65310a620095e2e
Nathanael Ferry P <ntnferry1505@gmail.com>
2018-10-13 14:24:21 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,16 +9,18 @@ echo -e "\e[1;36m
  |_| \_| |_| |_| |_/_/   \_\_| \_|_|   |_|    
                                               "
 sleep 1
-echo "===================== CLI Tools ======================";
-echo "================ NTHANFP.ME | axec0de ================";
-echo -e "==================== Version 0.1 =====================\e[0m";
+echo "==== Instagram Tools Ver ".version()." ====";
+echo "============ Code by ============";
+echo -e "======= NTHANFP | AXEC0DE =======\e[0m";
 sleep 1
 echo ""
 echo -e "\e[1;35m[!] Select tools by number : "
-echo "1) Spotify Checker"
-echo "2) Spin to Cash Mining"
-echo "3) Mass Check Username Insta"
-echo "4) Mass Check HTTP Code"
+echo "1) Auto Mention"
+echo "2) Auto Like Timeline"
+echo "3) Delete All Media"
+echo "4) Follow Target"
+echo "5) Find Firts Posting"
+echo "6) Unfollow Who Not Followback"
 echo -e "0) keluar\e[0m"
 echo -e "\e[1;32m"
 read -p "root@CLI-tools : " bro
@@ -27,30 +29,44 @@ echo -e "\e[0m"
 if [ $bro = 1 ] || [ $bro = 1 ]
 then
 clear
-cd spotifyCheck
-php run.php
+cd tools
+php automention.php
 fi
 
 if
 [ $bro = 2 ] || [ $bro = 2 ]
 then
 clear
-cd spinToCash
-php run.php
+cd tools
+php botlike.php
 fi
 
 if [ $bro = 3 ] || [ $bro = 3 ]
 then
 clear
-cd checkUsernameIG
-php run.php
+cd tools
+php deleteallmedia.php
 fi
 
 if [ $bro = 4 ] || [ $bro = 4 ]
 then
 clear
-cd HTTPheader
-php run.php
+cd tools
+php fft.php
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+cd tools
+php firtsmedia.php
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+cd tools
+php unfollownotfollback.php
 fi
 
 if [ $bro = 0 ] || [ $bro = 00 ]
